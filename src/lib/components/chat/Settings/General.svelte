35016c0581109b7fc6df2c4a2e073a9499f42854
zalazium <zalazium@zalazium.de>
2024-12-02 17:06:22 +0100

    cicd
    cicd
    
    cicd
    
    Update .gitignore to exclude translation files
    
    Fixing the popup and helping control to not be shown
    
    Node changes for version
    
    revert changes for .gitignore
    
    Revert testing yaml file
    
    Fixing the nodejs build failing
    
    Fixes to now show the update popups
    
    Changing the name and icon
    
    Updating the favicon

--- src/lib/components/chat/Settings/General.svelte
+++ src/lib/components/chat/Settings/General.svelte
@@ -199,7 +199,7 @@
 					</select>
 				</div>
 			</div>
-			{#if $i18n.language === 'en-US'}
+			<!-- {#if $i18n.language === 'en-US'}
 				<div class="mb-2 text-xs text-gray-400 dark:text-gray-500">
 					Couldn't find your language?
 					<a
@@ -210,7 +210,7 @@
 						Help us translate Open WebUI!
 					</a>
 				</div>
-			{/if}
+			{/if} -->
 
 			<div>
 				<div class=" py-0.5 flex w-full justify-between">
