ccd8117548bb497fb9b2853284ee66d4a41bfac5
zalazium <zalazium@zalazium.de>
2024-12-02 17:06:22 +0100

    cicd
    cicd
    
    cicd
    
    Update .gitignore to exclude translation files
    
    Fixing the popup and helping control to not be shown
    
    Node changes for version
    
    revert changes for .gitignore
    
    Revert testing yaml file
    
    Fixing the nodejs build failing
    
    Fixes to now show the update popups
    
    Changing the name and icon
    
    Updating the favicon

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -40,9 +40,7 @@
 
 	import Sidebar from '$lib/components/layout/Sidebar.svelte';
 	import SettingsModal from '$lib/components/chat/SettingsModal.svelte';
-	import ChangelogModal from '$lib/components/ChangelogModal.svelte';
 	import AccountPending from '$lib/components/layout/Overlay/AccountPending.svelte';
-	import UpdateInfoToast from '$lib/components/layout/UpdateInfoToast.svelte';
 
 	const i18n = getContext('i18n');
 
@@ -217,19 +215,6 @@
 </script>
 
 <SettingsModal bind:show={$showSettings} />
-<ChangelogModal bind:show={$showChangelog} />
-
-{#if version && compareVersion(version.latest, version.current) && ($settings?.showUpdateToast ?? true)}
-	<div class=" absolute bottom-8 right-8 z-50" in:fade={{ duration: 100 }}>
-		<UpdateInfoToast
-			{version}
-			on:close={() => {
-				localStorage.setItem('dismissedUpdateToast', Date.now().toString());
-				version = null;
-			}}
-		/>
-	</div>
-{/if}
 
 <div class="app relative">
 	<div
